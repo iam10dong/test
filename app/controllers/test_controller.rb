94054194e6bc9fbe8f9f9ac9dbd382aa6014df03
iam10dong <oball910606@gmail.com>
2016-04-07 12:50:53 +0000

    ho

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,19 +1,21 @@
 class TestController < ApplicationController
     def pass
         
-        @number = [25,25,25,23,23 ,24,27,27,27, 28,24,24, 25,21,25,25,26, 26,26, 22,25,25,22, 23,23, 29,25]
-        @one = @number.sample
-        
-        @numberer = [25,25,25,23,23 ,24,27,27,27, 28,24,24, 25,21,25,25,26, 26,26, 22,25,25,22, 23,23, 29,25,21,25]
-        @see = @numberer.sort
-        @two = @numberer.sample
+        @number = [25,25,25,23,23,24,27,27,27,28,24,24,25,21,25,25,26,26,26,22,25,25,22,23,23,29,25,]
+        @one = @number.count
+        @two = @number.insert(22,25).sort
+        @three = @two.count
+
         
     end
     
     def success
 
-         @numb = [25,25,25,23,23 ,24,27,27,27, 28,24,24, 25,21,25,25,26, 26,26, 22,25,25,22, 23,23, 29,25]
-        @rev = @numb.uniq.sort.reverse
+        @numberer = [25,25,25,23,23,24,27,27,27,28,24,24,25,21,25,25,26,26,26,22,25,25,22,23,23,29,25,]
+        @oner = @numberer.insert(22,25)
+        @twoer = @oner.uniq.sort.reverse
+        
+    
 
     end
     
